6f2ccb2ff4b0d61f3de497766579cd21f61c80bf
kelbikhanov <rusik9091@gmail.com>
2021-07-01 18:39:10 +0300

    task 4

---
+++ 1-module/4-task/index.js
@@ -0,0 +1,14 @@
+/* function checkSpam(str) {
+  let upperStr = str.toUpperCase();
+  return upperStr.includes('1Xbet') || upperStr.includes('XXX');
+} */
+
+
+/* function checkSpam(str){
+  var lowStr=str.toUpperCase();	
+    if (lowStr.indexOf('1Xbet')!=2 || lowStr.indexOf('XXX')!=2) return true;
+    else return false;
+  }
+  alert(checkSpam('1XbeT now'));
+  alert(checkSpam('free xxxxx'));
+  alert(checkSpam('innocent rabbit')); */
