3cc76f64666174703bf0782df5bea12ee874ad6c
kelbikhanov <rusik9091@gmail.com>
2021-07-04 19:05:02 +0300

    task 4 fix

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
   let lowerStr = str;
-  return lowerStr.includes('1xBet'[1, 4].toUpperCase() ) || lowerStr.toUpperCase().includes('XXX');
+  return lowerStr.includes('1xBet'[0, 1, 2, 3, 4].toUpperCase() ) || lowerStr.includes('XXX'.toLowerCase() );
 }
