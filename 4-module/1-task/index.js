88fbe18be98652c24ed562db5d812bd6cc2d380d
kelbikhanov <rusik9091@gmail.com>
2021-07-24 11:52:02 +0300

    fix task 2

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,18 @@
+let friends = [
+  {
+      firstName: 'Artsiom',
+      lastName: 'Mezin'
+  },
+  {
+      firstName: 'Ilia',
+      lastName: 'Kantor'
+  },
+  {
+      firstName: 'Christopher',
+      lastName: 'Michael'
+  }
+];
+
 function makeFriendsList(friends) {
   const ul = document.createElement('ul');
 
