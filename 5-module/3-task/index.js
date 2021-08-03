74757a7098d4dc0a8f4a4fc2ec72e06b5a1b1deb
kelbikhanov <rusik9091@gmail.com>
2021-08-03 21:08:52 +0300

    module-5 task-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,59 @@
 function initCarousel() {
-  // ваш код...
-}
+
+  let position = 0;
+  const slidesToShow = 1;
+  const slidesToScroll = 1;
+  const container = document.querySelector('.carousel');
+  const track = document.querySelector('.carousel__inner');
+  const btnPrev = document.querySelector('.carousel__arrow_left');
+  const btnNext = document.querySelector('.carousel__arrow_right');
+  const items = document.querySelectorAll('.carousel__slide');
+  const itemsCount = items.length;
+  let visible = true;
+
+  const itemWidth = track.clientWidth / slidesToShow;
+  const movePosition = slidesToScroll * itemWidth;
+
+  items.forEach((item) => { 
+    item.style.minWidth = `${itemWidth}px`;
+  });
+  
+  btnNext.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+      
+    setPosition();
+    checkBtns();
+  });
+
+  btnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+  });
+
+  const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`;
+  };
+
+  const checkBtns = () => {
+    if(position === 0) {
+      btnPrev.style.display = 'none';
+      visible = false;
+    } else {
+      btnPrev.style.display = '';
+      visible = true;
+    }
+
+    if(position <= -(itemsCount - slidesToShow) * itemWidth) {
+      btnNext.style.display = 'none';
+      visible = false;
+    } else {
+      btnNext.style.display = '';
+      visible = true;
+    }
+  };
+  checkBtns();
+};
